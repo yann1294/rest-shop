b4548a8dab6f4c636aa1ca5a5e92f8b1e6aed165
yann1294 <dev.yann12@gmail.com>
2020-03-27 02:19:15 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect(
+    'mongodb+srv://shop-admin:'
+    +
+    process.env.MONGO_ATLAS_PW
+    +
+    '@node-rest-shop-votqu.mongodb.net/test?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Handling CORS
+app.use((req, res, next) =>{
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-type, Accept, Authorization');
+   if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH', 'DELETE, GET');
+        return res.status(200).json({});
+   }
+   next();
+});
+
+
+// Routes which handles requests
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+
+// Routes Handling errors
+app.use((req, res, next) =>{
+   const error = new Error('Not found');
+   error.status = 404;
+   next(error);
+});
+
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
+
+module.exports = app;

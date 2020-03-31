3e2c54b409bcc946fa77d644498fe06b194deb39
yann1294 <dev.yann12@gmail.com>
2020-03-31 18:39:38 +0530

    Populating Queries.

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -10,6 +10,7 @@ const Product = require('../models/product');
 router.get('/', (req, res, next) =>{
    Order.find()
        .select('product quantity _id')
+       .populate('product', 'name')
        .exec()
        .then(docs => {
            res.status(200).json({
@@ -75,7 +76,9 @@ router.post('/', (req, res, next) =>{
 });
 
 router.get('/:orderId', (req, res, next) =>{
-   Order.findById(req.params.orderId).exec()
+   Order.findById(req.params.orderId)
+       .populate('product')
+       .exec()
        .then(order => {
            if (!order){
                return res.status(404).json({
